7fd30bca2e934938d4874cdf2029f477b6c31ce6
natsu1211 <longyutao1211@gmail.com>
2016-07-20 19:39:17 +0900

    add oh-my-zsh install script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -2,6 +2,10 @@
 #author: natsu1211
 
 DIR=`cd $(dirname $0); pwd`
+if ! [ -d $HOME/.oh-my-zsh ] ; then
+    cd $HOME
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 for file in zshrc; do
     if [ -e $HOME/.$file ]; then
         old=$file${str}_old
