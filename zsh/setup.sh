abb5c6cf50f6e9cdea1c20358bbe884d759110f8
natsu1211 <longyutao1211@gmail.com>
2016-08-02 15:50:38 +0900

    add zsh setup script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,17 +1,36 @@
 #!/bin/sh
 #author: natsu1211
 
-DIR=`cd $(dirname $0); pwd`
-if ! [ -d $HOME/.oh-my-zsh ] ; then
-    cd $HOME
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+set -eu
+slink() {
+	[ -e "$2" ] || ln -sf "$1" "$2"
+}
+has() {
+	type "$1" > /dev/null 2>&1
+}
+
+#install zsh
+if ! has zsh; then
+	if [ "$(uname)" = "Darwin" ]; then
+		brew install zsh --disable-etcdir
+		brew install zsh-completions
+	elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
+		sudo apt-get install zsh
+	fi
 fi
+#install oh-my-zsh
+if has zsh; then
+	if [ ! -d $HOME/.oh-my-zsh ] ; then
+	    cd $HOME
+	    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	fi
+fi
+DIR=`cd "$(dirname $0)"; pwd`
 for file in zshrc; do
     if [ -e $HOME/.$file ]; then
-        old=$file${str}_old
+        old=${file}_old
         mv $HOME/.$file $HOME/.$old
     fi
 
-    ln -sf $DIR/.$file $HOME/.$file
+    slink $DIR/.$file $HOME/.$file
 done
-
