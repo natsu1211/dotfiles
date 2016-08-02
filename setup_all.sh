e7a1aa6d2e40385671c1a77281b3934a094451ac
natsu1211 <longyutao1211@gmail.com>
2016-08-02 15:50:52 +0900

    setup homebrew first

--- setup_all.sh
+++ setup_all.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #author: natsu1211
-
-for folder in atom git vim zsh tmux
+set -eu
+#install homebrew
+if [ "$(uname)" = "Darwin" ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+for folder in atom git vim zsh tmux ruby
 do
     cd $folder
     sh setup.sh
