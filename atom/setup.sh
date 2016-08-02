1cbf1d01310df2eb0fc6ad9985551c2b007e4f30
natsu1211 <longyutao1211@gmail.com>
2016-08-02 18:00:06 +0900

    link setting

--- atom/setup.sh
+++ atom/setup.sh
@@ -6,7 +6,11 @@ set -eu
 has() {
 	type "$1" > /dev/null 2>&1
 }
-
+ATOM_HOME=$HOME/.atom
+DIR=`cd $(dirname $0); pwd`
+slink() {
+	[ -e "$2" ] || ln -sf "$1" "$2"
+}
 #install Atom
 cd $HOME
 if [ "$(uname)" = "Darwin" ]; then
@@ -16,3 +20,17 @@ elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
   #has atom || curl -OL https://github.com/atom/atom/releases/download/v1.9.0/atom-amd64.tar.gz && tar -xzf atom-amd64.tar.gz
   sudo apt-get install atom
 fi
+
+#clone from gist
+git clone https://gist.github.com/c75b07894f099c5ffe2a8b9a7513373c.git atom-settings && cd atom-settings
+for file in init.coffee keymap.cson settings.json snippets.cson styles.less
+do
+    if [ -e $ATOM_HOME/.$file ]; then
+        old=${file}_old
+        mv $ATOM_HOME/.$file $ATOM_HOME/.$old
+    fi
+
+    slink $DIR/.$file $ATOM_HOME/.$file
+done
+#install sync-setting plugin
+apm install sync-setting
