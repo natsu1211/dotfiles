f1b5c107a480e07aaa8f2823fa62b10bfdecfe8f
natsu1211 <longyutao1211@gmail.com>
2017-07-28 02:30:11 +0900

    update tmux config

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,33 +1,6 @@
 #!/bin/sh
 #author: natsu1211
-
-set -eu
-has() {
-	type "$1" > /dev/null 2>&1
+init_target tmux
+process_tmux(){
+	symlink tmux/.tmux.conf .tmux.conf
 }
-
-slink() {
-    [ -e "$2" ] || ln -sf "$1" "$2"
-}
-#install tmux
-if ! has tmux; then
-	if [ "$(uname)" = "Darwin" ]; then
-		brew install tmux
-	elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
-    sudo apt-get install tmux
-	fi
-fi
-
-#link
-set -eu
-DIR=`cd $(dirname $0); pwd`
-for file in tmux.conf; do
-    if [ -e $HOME/.$file ]; then
-        old=${file}_old
-        mv $HOME/.$file $HOME/.$old
-    fi
-
-    slink $DIR/.$file $HOME/.$file
-done
-
-
