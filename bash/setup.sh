859270b846f67ac9cd8c22a9a87283ea02a65605
natsu1211 <longyutao1211@gmail.com>
2017-07-28 02:29:57 +0900

    update bash config

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-#author: natsu1211
 
-set -eu
-slink() {
-	[ -e "$2" ] || ln -sf "$1" "$2"
+init_target bash
+process_bash(){
+	symlink bash/.bashrc .bashrc
 }
-DIR=`cd $(dirname $0); pwd`
-for file in bashrc; do
-    if [ -e $HOME/.$file ]; then
-        old=${file}_old
-        mv $HOME/.$file $HOME/.$old
-    fi
-
-    slink $DIR/.$file $HOME/.$file
-done
-
