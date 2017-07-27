2f3ced8e0b6dc6ebc39d5fd464a5ff3efac85444
natsu1211 <longyutao1211@gmail.com>
2017-07-28 02:28:12 +0900

    update atom config

--- atom/setup.sh
+++ atom/setup.sh
@@ -1,39 +1,10 @@
 #!/bin/sh
-#author: natsu1211
-
-set -eu
-
-has() {
-	type "$1" > /dev/null 2>&1
+init_target atom
+process_atom(){
+	if [ -d $HOME/.atom/ ]; then
+		for file in *.[!s][!h]*
+		do
+			symlink atom/$file .atom/$file
+		done
+	fi
 }
-ATOM_HOME=$HOME/.atom
-ATOM_SETTINGS=atom-settings
-DIR=`cd $(dirname $0); pwd`
-slink() {
-	[ -e "$2" ] || ln -sf "$1" "$2"
-}
-#install Atom
-cd $HOME
-if [ "$(uname)" = "Darwin" ]; then
-  #has atom || curl -OL https://github.com/atom/atom/releases/download/v1.9.0/atom-mac.zip && unzip atom-mac.zip && mv atom.app $HOME/Applications
-  has atom || brew install caskroom/cask/brew-cask
-	has atom || brew cask install atom
-elif [ "$(expr substr "$(uname -s)" 1 5)" = "Linux" ]; then
-  #has atom || curl -OL https://github.com/atom/atom/releases/download/v1.9.0/atom-amd64.tar.gz && tar -xzf atom-amd64.tar.gz
-  sudo apt-get install atom
-fi
-cd $DIR
-#clone from gist
-if [ -d atom-settings ]; then
-	git submodule add https://gist.github.com/c75b07894f099c5ffe2a8b9a7513373c.git $ATOM_SETTINGS
-	cd atom-settings
-	for file in init.coffee keymap.cson settings.json snippets.cson styles.less
-	do
-		if [ -e $ATOM_HOME/$file ]; then
-			old=${file}_old
-			mv $ATOM_HOME/$file $ATOM_HOME/$old
-		fi
-
-		slink $DIR/$ATOM_SETTINGS/$file $ATOM_HOME/$file
-	done
-fi
