d1e3a2c7aa5a133e1c81d767b7dc2e292ff3387b
natsu1211 <longyutao1211@gmail.com>
2017-07-28 16:16:41 +0900

    fix setup script and add prerequisite for atom

--- pre/setup.sh
+++ pre/setup.sh
@@ -2,7 +2,7 @@
 
 process_pre(){
     #install homebrew
-    if [ "$(uname)" = "Darwin" ]; then
+    if ! has brew; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
     #install zsh
@@ -17,7 +17,7 @@ process_pre(){
     #install oh-my-zsh
     if has zsh; then
         if [ ! -d $HOME/.oh-my-zsh ]; then
-            cd $HOME
+            cd $HOME  || return
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         fi
     fi
@@ -25,24 +25,29 @@ process_pre(){
     if ! has anyenv; then
         git clone https://github.com/riywo/anyenv ~/.anyenv
         cp -f $HOME/github/dotfiles/.zshrc $HOME/.zshrc
+	source $HOME/.zshrc
+	$SHELL -l
         if ! has rbenv; then
             anyenv install rbenv
+            $SHELL -l
             RECOMMENDED_RB_VAR="2.4.1"
             rbenv install $RECOMMENDED_RB_VAR
             rbenv global $RECOMMENDED_RB_VAR
+            rbenv rehash
             gem install bundler
             gem install rubocop
-            rbenv rehash
         fi
         if ! has plenv; then
             anyenv install plenv
-            RECOMMENDED_PERL_VAR="5.26.0"
+            $SHELL -l
+            RECOMMENDED_PERL_VAR="5.27.2"
             plenv install $RECOMMENDED_PERL_VAR
             plenv global $RECOMMENDED_PERL_VAR
             plenv rehash
         fi
         if ! has pyenv; then
             anyenv install pyenv
+            $SHELL -l
             RECOMMENDED_PY_VAR="3.6.2"
             pyenv install $RECOMMENDED_PY_VAR
             pyenv global $RECOMMENDED_PY_VAR
@@ -50,6 +55,7 @@ process_pre(){
         fi
         if ! has goenv; then
             anyenv install goenv
+            $SHELL -l
             RECOMMENDED_GO_VAR="1.8.3"
             goenv install $RECOMMENDED_GO_VAR
             goenv global $RECOMMENDED_GO_VAR
@@ -75,7 +81,7 @@ process_pre(){
     #install NeoVim
 	if [ "$(uname)" = "Darwin" ]; then
         has nvim || brew install neovim
-        sudo pip2 install --upgrade neovim
+        sudo pip install --upgrade neovim
         sudo pip3 install --upgrade neovim
     fi
 	#install xvim
@@ -83,7 +89,7 @@ process_pre(){
         XVIM_HOME=$HOME/downloads/xvim
         if [ ! -d XVIM_HOME ]; then
             git clone https://github.com/XVimProject/XVim.git $XVIM_HOME
-            cd $XVIM_HOME
+            cd $XVIM_HOME || return
             make
         fi
     fi
@@ -94,6 +100,7 @@ process_pre(){
     if ! has nvm; then
         #install nvm, nodejs, npm
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+        source ~/.zshrc
         nvm install node
         npm install -g typescript
     fi
@@ -104,15 +111,23 @@ process_pre(){
     # jedi
     pip install jedi
     # font
-    FONT_DIR=$HOME/powerline-font-dir
-    git clone https://github.com/powerline/fonts.git $FONT_DIR
-    cd $FONT_DIR
+    FONT_DIR=$HOME/fonts
+    cd $HOME || return
+    git clone https://github.com/powerline/fonts.git
+    cd $FONT_DIR || return
     ./install.sh
-    cd ..
-    rm -rf $FONT_DIR
-
-
-
-
-
+    cd $HOME || return
+    # tig
+    brew install tig
+    # pylint
+    if has python; then
+        python -m pip --upgrade install astroid
+        python -m pip --upgrade install isort
+        git clone https://github.com/PyCQA/pylint.git
+        cd pylint || return
+        python setup.py install
+        # autopep
+        python -m pip install --upgrade pycodestyle
+        python -m pip install --upgrade autopep8
+    fi
 }
