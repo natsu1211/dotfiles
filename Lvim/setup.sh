0ba3a29b09fb80ba7a6d7ee9b09380883dc4685a
natsu1211 <longyutao1211@gmail.com>
2017-07-29 01:10:23 +0900

    update vim setup setting

--- Lvim/setup.sh
+++ Lvim/setup.sh
@@ -4,6 +4,11 @@ init_target Lvim
 process_Lvim(){
 	symlink Lvim/.vimrc .vimrc
 	symlink Lvim/.ycm_extra_conf.py .ycm_extra_conf.py
+	if [ ! -d "$HOME/.vim" ]; then
+		directory $HOME/.vim
+	fi
 	symlink Lvim/userautoload/ .vim/userautoload
 	symlink Lvim/template .vim/template
+	symlink Lvim/colors .vim/colors
+	symlink Lvim/autoload .vim/autoload
 }
